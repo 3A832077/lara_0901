a53315b3004202fe26367f859a261c4a3455db5d
chen shu ting <a.0066715@gmail.com>
2021-11-21 16:37:08 +0800

    更新資料

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,5 @@ Route::prefix('admin')->group(function () {
     Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');
     Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');
     Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
+	Route::patch('posts/{post}', [AdminPostsController::class,'update'])->name('admin.posts.update');
 });
