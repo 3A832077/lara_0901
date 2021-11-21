238d8e6aa4ffe5b2f54c33d1d90b3670b1780d18
chen shu ting <a.0066715@gmail.com>
2021-11-21 16:30:16 +0800

    設定頁面跳轉

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -26,6 +26,7 @@ class AdminPostsController extends Controller
 	public function store(Request $request)
     {
        Post::create($request->all());
+	   return redirect()->route('admin.posts.index');
    }
 
 }
