c2d6f5e75efc685927d06be15726b32a6eec32f5
chen shu ting <a.0066715@gmail.com>
2021-11-21 18:58:57 +0800

    前台路由

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,18 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('posts.index',$data);
     }
 
     public function show($id)
     {
         $data = ['id' => $id];
-
         return view('posts.show', $data);
+    }
+	
+	 public function edit($id)
+    {
+        $post=Post::find($id);
+        $data=['post'=>$post];
+        return view('posts.show',$data);
     }
 }
